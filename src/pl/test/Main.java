6f975be368264eaed1b562cd69c74169af4bca1a
Administrator <belhub24@gmail.com>
2021-10-11 10:35:11 +0200

    dodanie klasy main

---
+++ src/pl/test/Main.java
@@ -0,0 +1,11 @@
+package pl.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("blabla");
+
+        System.exit(0);
+    }
+}
